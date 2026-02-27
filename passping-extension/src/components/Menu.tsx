24ad959563d9f3bb7d115f0f2026a8e829de2d18
samlui1009 <sam_lui@hotmail.ca>
2026-02-26 22:46:48 -0500

    Starting on the homepage for extension

---
+++ passping-extension/src/components/Menu.tsx
@@ -0,0 +1,15 @@
+function Menu() {
+
+    return (
+      <>
+          <div>
+            <button></button>
+            <button>Open U-Pass Website</button>
+            <button></button>
+          </div>
+      </>
+    )
+  }
+  
+  export default Menu
+  
