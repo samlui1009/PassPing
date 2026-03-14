2abed2d363cbe687ffc5cf82b90afff8e9a57804
samlui1009 <sam_lui@hotmail.ca>
2026-03-14 14:09:01 -0400

    Submission form for settings complete

--- passping-extension/src/pages/Settings.tsx
+++ passping-extension/src/pages/Settings.tsx
@@ -1,26 +1,62 @@
 import { IoArrowBackCircle } from "react-icons/io5";
+import { FaSave } from "react-icons/fa";
+
+import "../stylesheets/Settings.css";
 
 type SettingsProps = {
-    goBack: () => void;
-}
+  goBack: () => void;
+};
 
-function Settings( {goBack} : SettingsProps) {
+function Settings({ goBack }: SettingsProps) {
   return (
     <>
       <div>
-        <button className="back-btn" onClick={goBack}>
-          <IoArrowBackCircle className="back-icon"></IoArrowBackCircle>
-        </button>
-        <h1>Settings</h1>
-        <form>
-            <label htmlFor="reminder-date">Reminder Date:</label>
-            <input type="number" id="reminder-date" name="reminder-date" />
-            <p>Reminders will start on the set date for each month if your pass isn't loaded.</p>
-            <label htmlFor="time">Reminder Time:</label>
-            <input type="time" id="reminder-time" name="reminder-time" min="00:00" max="23:59"/>
-            <label htmlFor="nag-toggle">Nag Until Completed</label>
-            <input type="checkbox" id="nag-toggle" name="nag-toggle" />
-            <button type="submit">Save Settings</button>
+        <div className="settings-header">
+          <button className="back-btn" onClick={goBack}>
+            <IoArrowBackCircle className="back-icon"></IoArrowBackCircle>
+            Go Back to Home
+          </button>
+        </div>
+
+        <div className="divider"></div>
+        <h1 className="settings-title">Settings</h1>
+
+        <form className="settings-form">
+          <label htmlFor="reminder-date">Reminder Date:</label>
+          <input
+            className="form-input"
+            type="number"
+            id="reminder-date"
+            name="reminder-date"
+            min="1"
+            max="31"
+            step="1"
+          />
+          <p className="addnl-notes">
+            <b>NOTE:</b> Reminders will start on the set date for each month if
+            your pass isn't loaded.
+          </p>
+          <label htmlFor="time">Reminder Time:</label>
+          <input
+            className="form-input"
+            type="time"
+            id="reminder-time"
+            name="reminder-time"
+            min="00:00"
+            max="23:59"
+          />
+          <label htmlFor="nag-toggle">Nag Until Completed:</label>
+          <input type="checkbox" id="nag-toggle" name="nag-toggle" />
+          <p className="addnl-notes">
+            <b>NOTE:</b> If enabled, you will receive pop-up reminders every day
+            until your pass is loaded.
+          </p>
+          <div className="save-btn-div">
+            <button type="submit" className="save-settings-btn">
+              <FaSave className="icon"></FaSave>
+              Save Your Settings
+            </button>
+          </div>
         </form>
       </div>
     </>
